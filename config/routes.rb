a01276bd6c434f88d66a0204c8edf0ba8b8757c1
sdavidson140 <sdavidson140@gmail.com>
2018-10-13 23:00:30 -0400

    must be authenticated to view items - make items#index root of app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :item_types
   devise_for :users
 
-  root to: 'item_types#index'
+  root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
