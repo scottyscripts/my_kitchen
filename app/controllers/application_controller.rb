34a212308bdd05a056dcb03a123e74fa6888618c
sdavidson140 <sdavidson140@gmail.com>
2018-10-13 20:33:39 -0400

    generate views - run migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # provided by devise
+  # authenticate user
+  #   before_action :authenticate_user!
+  # verify if a user is signed in
+  #   user_signed_in?
+  # current signed in user
+  #   current_user
+  # access session for this scope
+  #   user_session
 end
