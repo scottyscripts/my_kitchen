6ccd223cceb5484e781c507d0583fddd420553e4
sdavidson140 <sdavidson140@gmail.com>
2018-10-13 21:07:30 -0400

    scaffold Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_031116) do
+ActiveRecord::Schema.define(version: 2018_10_14_005856) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "days_until_expiration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
