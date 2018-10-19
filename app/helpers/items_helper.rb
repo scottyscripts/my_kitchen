2ef38b30e414b23991694677d3c375c34dad967f
sdavidson140 <sdavidson140@gmail.com>
2018-10-19 17:50:48 -0400

    helper method to colorize item dates on items index - help icon on items index

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,15 @@
 module ItemsHelper
+  def colorized_expiration_date(expiration_date)
+    class_name = if expiration_date > 7.days.from_now
+      "text-success"
+    elsif expiration_date > 3.days.from_now
+      "text-warning"
+    else
+      "text-danger"
+    end
+
+    formatted_date = expiration_date.strftime("%a %b #{expiration_date.day.ordinalize}")
+    element = "<div class=#{class_name}>#{formatted_date}</div>"
+    element.html_safe
+  end
 end
