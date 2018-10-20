1700d034a9bd4d212b41c51d74d55210dcef3a45
sdavidson140 <sdavidson140@gmail.com>
2018-10-20 18:41:07 -0400

    display quantity on item index page if it exists

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -12,4 +12,12 @@ module ItemsHelper
     element = "<div class=#{class_name}>#{formatted_date}</div>"
     element.html_safe
   end
+
+  def item_with_quantity(item)
+    if item.quantity > 1
+      "#{item.name} (#{item.quantity})"
+    else
+      "#{item.name}"
+    end
+  end
 end
